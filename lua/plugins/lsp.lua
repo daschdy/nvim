13a0d4bb67467eca0498173ce6d3436ad71d1739
daschdy <dustin.heither@gmail.com>
2024-12-10 19:43:50 +0100

    More QoL Plugins
    - completion
    - git
    - lsp
    - file navigation
    - todo

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,10 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
+        "williamboman/mason.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        "j-hui/fidget.nvim",
+        "stevearc/conform.nvim",
         {
             -- vim omnicompletions
             -- triggered with ctrl+x ctrl+o
@@ -15,8 +19,63 @@ return {
                 },
             },
         },
+        {
+            "williamboman/mason-lspconfig.nvim",
+            opts = {
+                auto_install = false
+            }
+        },
+
     },
+
     config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "",
+                    package_pending = "",
+                    package_uninstalled = "",
+                },
+            },
+            ensure_installed = {
+                "lua_ls",
+                "stylua",
+            },
+        })
+
+        require("fidget").setup({
+            notification = {
+                view = {
+                    stack_upwards = false,
+                },
+                window = {
+                    border = "none",
+                    align = "bottom",
+                    winblend = 0,
+                },
+            },
+        })
+
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local lspconfig = require("lspconfig")
+        local servers = {
+            "lua_ls",
+            "clangd",
+        }
+
+        for _, server in ipairs(servers) do
+            lspconfig[server].setup({
+                capabilities = capabilities,
+            })
+        end
+
+        require("conform").setup({
+            formatters_by_ft = {
+                lua = { "stylua" },
+                c = { "clang-format" },
+            },
+        })
+
         -- check servers with: :help lspconfig-all
         require("lspconfig").lua_ls.setup {}
 
