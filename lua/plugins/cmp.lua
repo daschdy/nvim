ad08700b8a70768ca68e5b736c9ce980ae5b0fa3
Dustin Heither <dustin.heither@gmail.com>
2025-01-29 11:23:41 +0100

    - Formatting - Markview Plugin

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,19 +3,19 @@ return {
         "github/copilot.vim",
     },
     {
-      'saghen/blink.cmp',
-      dependencies = 'rafamadriz/friendly-snippets',
-      version = '*',
-      opts = {
-        keymap = { preset = 'default' },
+        'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
+        version = '*',
+        opts = {
+            keymap = { preset = 'default' },
 
-        appearance = {
-          use_nvim_cmp_as_default = true,
-          nerd_font_variant = 'mono'
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = 'mono'
+            },
+
+            signature = { enabled = true }
+
         },
-
-        signature = { enabled = true }
-
-      },
     }
 }
