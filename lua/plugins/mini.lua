817cbc70a44f6af6b9d9f453d308a019494132f6
Dustin Heither <dustin.heither@gmail.com>
2025-01-31 10:49:12 +0100

    New Icons
    - switch to mini.icons
    - nvim-web-devicons not needed anymore

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,6 +1,9 @@
 return {
     "echasnovski/mini.nvim",
     config = function()
+        local icons = require "mini.icons"
+        icons.setup({})
+
         local statusline = require "mini.statusline"
         statusline.setup {
             use_icons = true
--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,7 @@
 return {
     "stevearc/oil.nvim",
     dependencies = {
-        "nvim-tree/nvim-web-devicons",
+        "echasnovski/mini.icons"
     },
     config = function()
         require("oil").setup({
