f404ef57c75d6d086ad81a506570707561e22900
Dustin Heither <dustin.heither@gmail.com>
2025-02-18 21:00:26 +0100

    make default colorscheme transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -43,12 +43,19 @@ return {
         -- end
     },
     {
-        "baliestri/aura-theme",
-        lazy = false,
-        priority = 1000,
-        config = function(plugin)
-            vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-            vim.cmd([[colorscheme aura-dark]])
-        end
+        -- "baliestri/aura-theme",
+        -- lazy = false,
+        -- priority = 1000,
+        -- config = function(plugin)
+        --     vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+        --     vim.cmd([[colorscheme aura-dark]])
+        -- end
+    },
+    {
+        -- Disable background color
+        vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' }),
+        vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' }),
+        vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' }),
+        vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' }),
     }
 }
