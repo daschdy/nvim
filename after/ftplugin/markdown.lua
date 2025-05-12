e7f5d7d22c1895246f2633ff954638d7d5598c6c
Dustin Heither <dustin.heither@gmail.com>
2025-05-12 21:17:42 +0200

    styling md files

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,6 @@
+vim.opt_local.wrap = true           -- Enable line wrap
+vim.opt_local.shiftwidth = 4        -- Indent with 4 spaces
+vim.opt_local.tabstop = 4           -- Show tabs as 4 spaces wide
+vim.opt_local.softtabstop = 4       -- Insert/delete 4 spaces when pressing tab/backspace
+vim.opt_local.expandtab = true      -- Convert tabs to spacesrint("markdown ftplugin loaded")
+
--- init.lua
+++ init.lua
@@ -1,8 +1,6 @@
 require("config.lazy")
 
-local key = vim.keymap
 local set = vim.opt
-
 set.relativenumber = true
 set.number = true
 set.tabstop = 4
@@ -26,18 +24,25 @@ set.updatetime = 200
 set.timeoutlen = 250
 set.cmdheight = 0
 
+local key = vim.keymap
 key.set("n", "<space><space>r", "<cmd>source %<CR>")
 key.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 key.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover" })
 key.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 key.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "Go to references" })
-key.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
-key.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
-key.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics" })
-key.set("n", "<leader>{", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-key.set("n", "<leader>}", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+key.set("n", "<leader>crr", vim.lsp.buf.code_action, { desc = "Code action" })
+key.set("n", "<leader>crn", vim.lsp.buf.rename, { desc = "Rename" })
 key.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Format" })
+
+key.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+key.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+key.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open diagnostic float" })
+key.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostic to loclist" })
+
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-CR>", "copilot#Accept('<CR>')", { expr = true, silent = true, script = true })
+key.set("i", "<C-y>", "copilot#Accept('<CR>')", { expr = true, silent = true })
 
 key.set("n", "<leader>tt", "<cmd>split term://zsh", { desc = "Horizontal Terminal" })
 
@@ -64,9 +69,9 @@ vim.api.nvim_create_autocmd("TermClose", {
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
